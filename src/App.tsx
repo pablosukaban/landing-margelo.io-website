233a09d365b666f89792ff08f3c1f94a422ef46f
ilya <zapaska96@gmail.com>
2022-09-11 16:28:49 +0500

    styles dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Masthead from './components/masthead';
 import AboutUs from './components/AboutUs';
+import Skills from "./components/Skills";
 
 const App = () => {
     return (
         <div className="App">
             <Masthead />
             <AboutUs />
+            <Skills />
         </div>
     );
 };
